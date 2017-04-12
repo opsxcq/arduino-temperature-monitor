4834157f83a9353c5740675a129d8b2afd968f82
OPSXCQ <opsxcq@strm.sh>
2017-04-12 18:03:08 -0300

    API updated to persist into influxdb

--- backend/api.js
+++ backend/api.js
@@ -12,12 +12,27 @@ let db = new influx.InfluxDB({
     database: process.env.INFLUX_DATABASE || "temperature"
 });
 
+let info = winston.info;
+let error = winston.error;
+
 // Configure the body parsing routines
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true }));
 
 // Just print the incoming data
 api.post('/data', (req, res, next) => {
-    winston.info(req.body);
-    return res.sendStatus(201);
+    info(req.body);
+
+    // Persist data
+    db.writePoints([
+        measurement: 'data',
+        tags: { type: req.body.type },
+        fields: { device: req.body.device, sensor: req.body.sensor, value: req.body.value },
+        timestamp: new Date().getTime() * 1000000
+    ]).then(() => {
+        return res.sendStatus(201);
+    }).catch( err => {
+        error(err.message);
+        return res.sendStatus(500);
+    });
 });
